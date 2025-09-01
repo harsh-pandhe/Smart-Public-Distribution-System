722fde23d008da226d787bf3c3a8a9eb9d84d4a4
Harsh Pandhe <harshpandhehome@gmail.com>
2025-09-02 03:27:54 +0530

    feat: remove smart-pds-backend and add new contracts and tests
    - Deleted the smart-pds-backend directory including package.json, deploy script, transaction script, test files, and tsconfig.json.
    - Introduced new Counter and Token contracts with corresponding tests.
    - Added a Hardhat configuration file for the new setup.
    - Implemented a new Ignition module for the Counter contract.
    - Created a new script for sending transactions on the OP chain.
    - Updated test files to validate the functionality of the new contracts.

--- smart-pds-backend/ignition/modules/RationDistribution.ts
+++
@@ -1,8 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("RationDistributionModule", (m) => {
-    const rationDistribution = m.contract("RationDistribution");
-    return {
-        rationDistribution
-    };
-});
